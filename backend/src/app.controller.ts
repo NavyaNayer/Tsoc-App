6f9f74592c84b306801c61ea35f5ae6da7e2c1ff
Navya Nayer <navya.nayer03@gmail.com>
2024-07-28 19:56:56 +0530

    Add frontend and update backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('api/data')
+  getData(): string {
+    return 'Hello from backend!';
+  }
 }
+
+
